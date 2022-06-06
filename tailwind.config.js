65c2f765536ee774973b4109215d9ff719456697
Dancingkoalaa <tim2001.tw@gmail.com>
2022-06-07 00:13:02 +0200

    InitialCommit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        jet: '#39332d',
+        slategray: '#6e8087',
+        rustyred: '#d83148',
+        independence: '#38405f',
+        independence2: '#59546c'
+        }
+    },
+  },
+  plugins: [],
+}
